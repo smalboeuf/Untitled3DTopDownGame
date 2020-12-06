07b0028d549e74fcf564b24ebed643e153a7c6b7
Sheldon Malboeuf <sheldonm07@gmail.com>
2020-12-05 20:11:59 -0500

    first commit

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
+
+namespace RPG.Combat
+{
+  public class Fighter : MonoBehaviour
+  {
+
+    [SerializeField] float weaponRange = 2f;
+
+    Transform target;
+
+    private void Update()
+    {
+
+      if (target == null) return;
+
+      if (target != null && !GetIsInRange())
+      {
+        GetComponent<Mover>().MoveTo(target.position);
+      }
+      else
+      {
+        GetComponent<Mover>().Stop();
+      }
+    }
+
+    private bool GetIsInRange()
+    {
+      return Vector3.Distance(transform.position, target.position) < weaponRange;
+    }
+
+    public void Attack(CombatTarget combatTarget)
+    {
+      GetComponent<ActionScheduler>().StartAction(this);
+      target = combatTarget.transform;
+    }
+
+    public void Cancel()
+    {
+      target = null;
+    }
+  }
+
+}
+
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using RPG.Combat;
+using RPG.Core;
+
+namespace RPG.Movement
+{
+  public class Mover : MonoBehaviour
+  {
+
+    NavMeshAgent navMeshAgent;
+
+    public void Start()
+    {
+      navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      UpdateAnimator();
+    }
+
+    public void StartMoveAction(Vector3 destination)
+    {
+      GetComponent<ActionScheduler>().StartAction(this);
+      GetComponent<Fighter>().Cancel();
+      MoveTo(destination);
+    }
+
+    public void MoveTo(Vector3 destination)
+    {
+      navMeshAgent.destination = destination;
+      navMeshAgent.isStopped = false;
+    }
+
+    public void Stop()
+    {
+      navMeshAgent.isStopped = true;
+    }
+
+    private void UpdateAnimator()
+    {
+      Vector3 velocity = navMeshAgent.velocity;
+      Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+      float speed = localVelocity.z;
+      GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+
+    }
+  }
+}
