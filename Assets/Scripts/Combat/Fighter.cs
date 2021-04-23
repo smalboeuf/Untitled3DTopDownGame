f4cc4c061c906ba228a1a669aa6fc7fad4147b6a
Sheldon Malboeuf <sheldonm07@gmail.com>
2021-04-22 21:24:24 -0400

    Add weapons

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -10,19 +10,18 @@ namespace RPG.Combat
   public class Fighter : MonoBehaviour, IAction
   {
 
-    [SerializeField] float weaponRange = 2f;
     [SerializeField] float timeBetweenAttacks = 1f;
-    [SerializeField] float weaponDamage = 5f;
-    [SerializeField] GameObject weaponPrefab = null;
-    [SerializeField] Transform handTransform = null;
-    [SerializeField] AnimatorOverrideController weaponOverride = null;
+    [SerializeField] Transform rightHandTransform = null;
+    [SerializeField] Transform leftHandTransform = null;
+    [SerializeField] Weapon defaultWeapon = null;
 
     Health target;
     float timeSinceLastAttack = Mathf.Infinity;
+    Weapon currentWeapon = null;
 
     private void Start()
     {
-      SpawnWeapon();
+      EquipWeapon(defaultWeapon);
     }
 
     private void Update()
@@ -44,11 +43,11 @@ namespace RPG.Combat
       }
     }
 
-    private void SpawnWeapon()
+    public void EquipWeapon(Weapon weapon)
     {
-      Instantiate(weaponPrefab, handTransform);
+      currentWeapon = weapon;
       Animator animator = GetComponent<Animator>();
-      animator.runtimeAnimatorController = weaponOverride;
+      weapon.Spawn(rightHandTransform, leftHandTransform, animator);
     }
 
 
@@ -73,12 +72,12 @@ namespace RPG.Combat
     void Hit()
     {
       if (target == null) return;
-      target.TakeDamage(weaponDamage);
+      target.TakeDamage(currentWeapon.GetDamage());
     }
 
     private bool GetIsInRange()
     {
-      return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
+      return Vector3.Distance(transform.position, target.transform.position) < currentWeapon.GetRange();
     }
 
     public bool CanAttack(GameObject combatTarget)
---
+++ Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+  [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Make New Weapon", order = 0)]
+  public class Weapon : ScriptableObject
+  {
+    [SerializeField] AnimatorOverrideController animatorOverride = null;
+    [SerializeField] GameObject equippedPrefab = null;
+    [SerializeField] float weaponDamage = 5f;
+    [SerializeField] float weaponRange = 2f;
+    [SerializeField] bool isRightHanded = true;
+
+    public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
+    {
+      if (equippedPrefab != null)
+      {
+        Transform handTransform;
+        if (isRightHanded)
+        {
+          handTransform = rightHand;
+        }
+        else
+        {
+          handTransform = leftHand;
+        }
+
+        Instantiate(equippedPrefab, handTransform);
+      }
+      if (animatorOverride != null)
+      {
+        animator.runtimeAnimatorController = animatorOverride;
+      }
+    }
+
+    public float GetDamage()
+    {
+      return weaponDamage;
+    }
+
+    public float GetRange()
+    {
+      return weaponRange;
+    }
+  }
+}
